bd6c30578fdaad831dacd366b2e0aa444e038a25
Chris Cesare <chris.cesare@gmail.com>
2018-12-21 11:22:55 -0500

    Added bandit4 solution

---
+++ bandit4/bandit4.py
@@ -0,0 +1,30 @@
+from pwn import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--interactive",
+                    help="open an interactive shell on host",
+                    action="store_true")
+args = parser.parse_args()
+
+BANDIT = 4
+
+with open("../bandit" + str(BANDIT - 1) + "/password", "r") as old_pass:
+    pw = old_pass.read().replace("\n", "")
+
+server = "bandit.labs.overthewire.org"
+server_port = 2220
+user = "bandit" + str(BANDIT)
+
+conn = ssh(user, server, password=pw, port=server_port)
+
+if args.interactive:
+    conn.interactive()
+else:
+    payload = "file **/* | grep ASCII | sed \"s/:.*//\" | xargs cat"
+    password = conn.process(payload, shell=True).recvall()
+
+    with open("password", "w") as pass_file:
+        pass_file.write(password)
+
+    print password.replace("\n", "")
